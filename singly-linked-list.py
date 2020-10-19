f7d5d6610f54c83aa201b1e74821150baec95a78
Daniel Lee <danieljplee1@gmail.com>
2020-10-19 01:40:07 -0400

    added logic to the if/else on the insertAfter function. testing must be done.

--- singly-linked-list.py
+++ singly-linked-list.py
@@ -1,81 +1,85 @@
 class SinglyLinkedList:
-  """LIFO Stack implementation using a singly linked list for storage."""
+    """LIFO Stack implementation using a singly linked list for storage."""
 
  #-------------------------- nested _Node class --------------------------
-  class _Node:         # "inner" class
-    """Lightweight, nonpublic class for storing a singly linked node."""
-    __slots__ = '_element', '_next'           # streamline memory usage
+    class _Node:         # "inner" class
+        """Lightweight, nonpublic class for storing a singly linked node."""
+        __slots__ = '_element', '_next'           # streamline memory usage
 
-    def __init__(self, element, next):        # initialize node's fields
-        self._element = element               # reference to user's element
-        self._next = next                     # reference to next node
+        def __init__(self, element, next):        # initialize node's fields
+            self._element = element               # reference to user's element
+            self._next = next                     # reference to next node
 
-    def setNext(self, next):
-        self._next = next
+        def setNext(self, next):
+            self._next = next
     
-    def getNext(self):
-        return self._next
+        def getNext(self):
+            return self._next
   
-    def setElement(self, element):
-        self.element = _element
+        def setElement(self, element):
+            self.element = _element
     
-    def getElement(self):
-        return self._element
+        def getElement(self):
+            return self._element
         
   #------------------------------- stack methods -------------------------------
-  def __init__(self, head = None, size = 0):
-    """Create an empty list."""
-    self._head = head                       # reference to the head node
-    self._size = size                       # number of stack elements
+    def __init__(self, head = None, size = 0):
+        """Create an empty list."""
+        self._head = head                       # reference to the head node
+        self._size = size                       # number of stack elements
 
-  def getSize(self):
-    """Return the number of elements in the list."""
-    return self._size
+    def getSize(self):
+        """Return the number of elements in the list."""
+        return self._size
 
-  def isEmpty(self):
-    """Return True if the list is empty."""
-    return self._size == 0
+    def isEmpty(self):
+        """Return True if the list is empty."""
+        return self._size == 0
 
-  def add (self, element):
-    """Add element element to the end of the list."""
-    node = self._Node(element, None)
-    if self.isEmpty():
-        self._head = node
-    else:
-        cursor = self._head
-        while cursor.getNext() is not None:
-            cursor = cursor.getNext( )
-        cursor.setNext(node)
-    print('Adding # ', self._size, ' element: ', node.getElement())
-    self._size += 1
+    def add (self, element):
+        """Add element element to the end of the list."""
+        node = self._Node(element, None)
+        if self.isEmpty():
+            self._head = node
+        else:
+            cursor = self._head
+            while cursor.getNext() is not None:
+                cursor = cursor.getNext( )
+            cursor.setNext(node)
+        print('Adding # ', self._size, ' element: ', node.getElement())
+        self._size += 1
         
-  def remove(self):
-    """Remove the element at the head of the list. Raise Empty if the list is empty."""
-    if self.isEmpty():
-      raise Empty('List is empty')
-    self._size -= 1
-    out = self._head.getElement()      # save the head element and return to the caller
-    self._head = self._head.getNext()  # Reset the head to the second (next) list element
-    return out
+    def remove(self):
+        """Remove the element at the head of the list. Raise Empty if the list is empty."""
+        if self.isEmpty():
+            raise Empty('List is empty')
+        self._size -= 1
+        out = self._head.getElement()      # save the head element and return to the caller
+        self._head = self._head.getNext()  # Reset the head to the second (next) list element
+        return out
 
-  def showList(self):
-    """Display the contents of the list, from head to tail. Raise Empty exception if the List is empty. """
-    if not self.isEmpty():
-        cursor = self._head
-        while True:
-            print(cursor.getElement(), ' reference: ', id(cursor), id(cursor.getNext()))
-            if cursor.getNext() == None: break
-            cursor = cursor.getNext()
-    return
+    def showList(self):
+        """Display the contents of the list, from head to tail. Raise Empty exception if the List is empty. """
+        if not self.isEmpty():
+            cursor = self._head
+            while True:
+                print(cursor.getElement(), ' reference: ', id(cursor), id(cursor.getNext()))
+                if cursor.getNext() == None: break
+                cursor = cursor.getNext()
+        return
         
-  def search(self, key):
-    """ Search the list using 'key. """        
-    cursor = self._head
-    while cursor:
-        if key == cursor.getElement():
-            return True
-        cursor = cursor.getNext()
-    return False
+    def search(self, key):
+        """ Search the list using 'key. """        
+        cursor = self._head #'cursor' is the node pointer, starting at the list head.
+        while cursor: #loop until cursor is 'None', the list tail was reached.
+            if key == cursor.getElement(): #is the node element equal to the 'key' search target?
+                return True
+            cursor = cursor.getNext() #point to the next node by setting cursor to next.
+        return False
+
+#"""The above searches the SLL one node at a time, starting with the head, until it finds a node whose element (data or key) equals the parameter 'key' value or it reaches the node after the tail, which is 'None'.  Lines 3, 4, and 7 are the way you loop through the list.  I've added some comments to help you understand what's going on, but the code itself was provided with the assignment as a Jupyter notebook file.  Copy it in and start from there if you haven't already done it.   """
+#"""Remember that a pointer ('cursor' in the above code) is the object reference - the long number representing the place in storage where the object's data is kept.  Each node is located at a difference place and is usually not in consecutive locations.  How to follow and manipulate these pointers is the thing I'm trying to get across here. """
+#"""For 'insertAfter' you have to keep a counter variable to count how many nodes you've passed before inserting after the ith one.  For 'insertElt' you have to save the previous and next node pointers as you loop through the list so you can place your new node after the previous or before the next one.  """
 
 #---------------------------------------------------------------------------------
 # IT-309 A4 requires you to complete the code for the two methods that follow
@@ -84,18 +88,34 @@ class SinglyLinkedList:
 # ^^^^^ SHOULD ONLY BE AROUND 30 LINES OF CODE FOR THESE TWO METHODS COMBINED
 # ^^^^^ Review how LINKED STRUCTURES work and OBJECT ID's for pointing elements
 # ^^^^^ Textbook pages 255-265 (chapter 7)
-  def insertAfter (self, i, element): # 'i' is the 'ith' element, 'element' is the element itself
-    """Insert an element after the ith element of the list."""
-    node = self._Node(element, None)
-  # insert method body below
-  # Cases: (1) insert into empty list, (2) insert before the head, (3) insert in the interior
+    def insertAfter (self, i, element): 
+        # 'i' is the 'ith' element (int input), 'element' is the element itself to be inserted (str input)
+        """Insert an element after the ith element of the list."""
+        node = self._Node(element, None)
+      # insert method body below
+      # Cases: (1) insert into empty list, (2) insert before the head, (3) insert in the interior
+        i = input(int)
+        # Case 1: insert into empty list
+        if self.isEmpty():
+            self._head = node # element?
+            # adds element to the empty list, making the element the head of the list. ***Case 1***
+        else:
+            cursor = self._i
+            if i >= 0:
+                cursor.setNext(node)
+            else:
+                self._head = node
+        
+            #pseudocode
+            # if 'i' is selected: add element after the 'i' element of the list. ***Case 3***
+            # else (if 'i' is not selected): add element before the head of the element. ***Case 2***
 
-  def insertElt (self, insrtThis, where, insrtHere):
-    """Insert element 'insrtThis' before/after node with element =='insrtHere'.
-       Parameter 'where' = "B" (before) or "A' (After) 'insrtHere'.        """
-    node = self._Node(insrtThis, None)
-    # insert method body below
-    # Hint: search for the list node element == 'insrtHere', then insert accordingly...'  
+    def insertElt (self, insrtThis, where, insrtHere):
+        """Insert element 'insrtThis' before/after node with element =='insrtHere'.
+           Parameter 'where' = "B" (before) or "A' (After) 'insrtHere'.        """
+        node = self._Node(insrtThis, None)
+        # insert method body below
+        # Hint: search for the list node element == 'insrtHere', then insert accordingly...'  
 
 class Empty(Exception):
     """Empty exception class provided to flag that condition. """
